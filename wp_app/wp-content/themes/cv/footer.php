dbdd69f15d73a0499b8cad552dc734cd7c8fd4b9
AresChris <148880863+AresChris@users.noreply.github.com>
2026-02-23 11:01:29 +0100

    cv terminé

---
+++ wp_app/wp-content/themes/cv/footer.php
@@ -0,0 +1,11 @@
+<!-- DEBUT FOOTER -->
+</main>
+
+<footer>Copyright ChrisAres</footer>
+    <?php get_template_part('templates/newsletter'); ?>
+<?php wp_footer(); ?>
+
+</body>
+</html>
+<!-- FIN FOOTER -->
+ 
---
+++ wp_app/wp-content/themes/cv/index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    get_header();
+    ?>
+
+    <div class="square">
+        <div class="name">
+            Christopher ARES
+        </div>
+        <div class="bandeau">
+            35 ans | Permis B | christopher.ares@icloud.com | 06.41.71.80.63
+        </div>
+        <div class="classe">
+            Ouvrier polyvalent
+        </div>
+        <div class="project">
+            Projet: développeur web et web mobile
+        </div>
+        <div class="bloc">
+            <div class="title">
+                Compétences liées à l'informatique
+            </div>
+
+
+            <div>
+                <ul>
+                    <li>Utilisation naturelle de l'outil informatique</li>
+                    <li>Connaissances et compréhension approfondies d'Excel</li>
+                    <li>Bases solides en Java, HTML/CSS</li>
+                </ul>
+            </div>
+            <div class="title">
+                Etudes / Formations
+            </div>
+            <div class="qualif">
+                <div class="date">2025</div>
+                <div class="qualif_1">Formation qualifiante développeur web</div>
+            </div>
+            <div class="prepa">
+                <div class="date">2023</div>
+                <div class="prepa_1">Formation préparatoire développeur web et web mobile</div>
+            </div>
+            <div class="bep">
+                <div class="date">2006/2008</div>
+                <div class="bep_1">BEP géomètre topographe en alternance</div>
+            </div>
+            <div class="bafa">
+                <div class="date">2009</div>
+                <div class="bafa_1">BAFA + AFPS</div>
+            </div>
+
+            <div class="title">
+                Experiences professionnelles
+            </div>
+            <div class="list">-Hotellerie/restauration | 2009/2010</div>
+            <div class="list">
+                <ul>
+                    <li>Plongeur</li>
+                    <li>Serveur/limonadier</li>
+                    <li>Crêpier et aide boulanger</li>
+                </ul>
+            </div>
+            <div class="list">-Bâtiment / travaux publics | 2010/2021</div>
+            <div class="list">
+                <ul>
+                    <li>Aide-maçon</li>
+                    <li>Scieur de béton</li>
+                    <li>Pose de resaux souterrains (fluvial, éléctrique...)</li>
+                    <li>Démolition</li>
+                    <li>Sollier moquettiste (pose de moquette et sols plastiques)</li>
+                </ul>
+            </div>
+            <div class="title">Employeurs</div>
+            <div class="readapt">
+                <div class="date">2025 | 2026</div>
+                <div class="readapt_1">Centre de réadaptation de Mulhouse</div>
+            </div>
+
+            <div>
+                <ul>
+                    <li>Développeur web et web mobile</li>
+                </ul>
+            </div>
+            <div class="agent_courrier">
+                <div class="date">2022 | 2023</div>
+                <div class="agent_courrier_1">La poste | Agent courrier</div>
+            </div>
+            <div>
+                <ul>
+                    <li>Tri et distribution de courrier aux entreprises</li>
+                </ul>
+            </div>
+            <div class="interim">
+                <div class="date">2019 | 2021</div>
+                <div class="interim_1">Adéquat 73 et Tremplin 73</div>
+            </div>
+            <div>
+                <ul>
+                    <li>Aide-maçon et scieur de béton</li>
+                </ul>
+            </div>
+            <div class="vinci">
+                <div class="date">2013 | 2016</div>
+                <div class="vinci_1">Vinci-Eurovia béton | Ouvrier polyvalent en rénovation d'ouvrages d'art</div>
+            </div>
+            <div>
+                <ul>
+                    <li>Réparation ponts et murs de soutènement (béton projeté)</li>
+                </ul>
+            </div>
+            <div class="nantet">
+                <div class="date">2010 | 2013</div>
+                <div class="nantet_1">Nantet | Démolition - déconstruction</div>
+            </div>
+
+            <div>
+                <ul>
+                    <li>Démolition/déconstruction manuelle</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    get_footer();
+    ?>
+</body>
+
+</html>
